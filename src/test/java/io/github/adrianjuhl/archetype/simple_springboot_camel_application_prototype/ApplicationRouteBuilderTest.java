ec235dedf9c17462d0ff97e81ffca8dbe03ad6cc
Adrian Juhl <adrian.juhl@gmail.com>
2023-01-09 16:23:28 +1030

    working - add appVersionInfo test

--- src/test/java/io/github/adrianjuhl/archetype/simple_springboot_camel_application_prototype/ApplicationRouteBuilderTest.java
+++ src/test/java/io/github/adrianjuhl/archetype/simple_springboot_camel_application_prototype/ApplicationRouteBuilderTest.java
@@ -67,7 +67,7 @@ class ApplicationRouteBuilderTest {
     mockOut.assertIsSatisfied();
     Message inMessage = mockOut.getExchanges().get(0).getIn();
     String actualBodyString = inMessage.getBody(String.class);
-    Assertions.assertEquals(MediaType.APPLICATION_JSON.toString(), inMessage.getHeader(Exchange.CONTENT_TYPE, String.class));
+    Assertions.assertEquals(MediaType.APPLICATION_JSON.toString(), inMessage.getHeader(Exchange.CONTENT_TYPE, String.class), "The value of the Content-Type header should be: " + MediaType.APPLICATION_JSON);
     Assertions.assertTrue(actualBodyString.startsWith("{"), "Body should start with {");
     Assertions.assertTrue(actualBodyString.endsWith("}"), "Body should end with }");
     Assertions.assertTrue(actualBodyString.contains("\"ping\":"), "Body should contain ping element");
@@ -78,40 +78,31 @@ class ApplicationRouteBuilderTest {
    *
    * @throws Exception
    */
-//  @Test
+  @Test
   public void appVersionInfoRouteShouldRespondWithAppVersionInfoMessage() throws Exception {
     MockEndpoint mockOut = getMockEndpoint("mock:out");
-//    AdviceWith.adviceWith(context, RouteIdentifier.PING.getRouteId(), a -> {
-//      a.weaveAddLast().to(mockOut.getEndpointUri());
-////      a.weaveAddLast().to("mock:out");
-//    });
-////    context
-////      .getRouteDefinition(RouteIdentifier.APP_VERSION_INFO.getRouteId())
-////      .adviceWith(context, new AdviceWithRouteBuilder() {
-////        @Override
-////        public void configure() throws Exception {
-////          weaveAddLast().to("mock:out");
-////        }
-////      });
-//    context.start();
-//    mockOut.expectedMessageCount(1);
-//    template.sendBody(RouteIdentifier.APP_VERSION_INFO.getRouteUri(), null);
-//    debugExchanges(mockOut);
-//    mockOut.assertIsSatisfied();
-//    Message inMessage = mockOut.getExchanges().get(0).getIn();
-//    String actualBodyString = inMessage.getBody(String.class);
-//    Assertions.assertEquals(MediaType.APPLICATION_JSON, inMessage.getHeader(Exchange.CONTENT_TYPE, String.class));
-//    Assertions.assertTrue(actualBodyString.startsWith("{"), "Body should start with {");
-//    Assertions.assertTrue(actualBodyString.endsWith("}"), "Body should end with }");
-//    Assertions.assertTrue(actualBodyString.contains("\"appVersionInfo\":"), "Body should contain appVersionInfo element");
-//    Assertions.assertTrue(actualBodyString.contains("\"projectGroupId\":"), "Body should contain projectGroupId element");
-//    Assertions.assertTrue(actualBodyString.contains("\"projectArtifactId\":"), "Body should contain projectArtifactId element");
-//    Assertions.assertTrue(actualBodyString.contains("\"projectVersion\":"), "Body should contain projectVersion element");
-//    Assertions.assertTrue(actualBodyString.contains("\"gitCommitHash\":"), "Body should contain gitCommitHash element");
-//    Assertions.assertTrue(actualBodyString.contains("\"gitCommitDatetime\":"), "Body should contain gitCommitDatetime element");
-//    Assertions.assertTrue(actualBodyString.contains("\"gitCommitTags\":"), "Body should contain gitCommitTags element");
-//    Assertions.assertTrue(actualBodyString.contains("\"gitCommitBranch\":"), "Body should contain gitCommitBranch element");
-//    Assertions.assertTrue(actualBodyString.contains("\"mvnBuildDatetime\":"), "Body should contain mvnBuildDatetime element");
+    AdviceWith.adviceWith(camelContext, RouteIdentifier.APP_VERSION_INFO.getRouteId(), a -> {
+      a.weaveAddLast().to(mockOut.getEndpointUri());
+    });
+    camelContext.start();
+    mockOut.expectedMessageCount(1);
+    template.sendBody(RouteIdentifier.APP_VERSION_INFO.getRouteUri(), null);
+    debugExchanges(mockOut);
+    mockOut.assertIsSatisfied();
+    Message inMessage = mockOut.getExchanges().get(0).getIn();
+    String actualBodyString = inMessage.getBody(String.class);
+    Assertions.assertEquals("application/json", inMessage.getHeader(Exchange.CONTENT_TYPE, String.class), "The value of the Content-Type header should be: application/json");
+    Assertions.assertTrue(actualBodyString.startsWith("{"), "Body should start with {");
+    Assertions.assertTrue(actualBodyString.endsWith("}"), "Body should end with }");
+    Assertions.assertTrue(actualBodyString.contains("\"appVersionInfo\":"), "Body should contain appVersionInfo element");
+    Assertions.assertTrue(actualBodyString.contains("\"projectGroupId\":"), "Body should contain projectGroupId element");
+    Assertions.assertTrue(actualBodyString.contains("\"projectArtifactId\":"), "Body should contain projectArtifactId element");
+    Assertions.assertTrue(actualBodyString.contains("\"projectVersion\":"), "Body should contain projectVersion element");
+    Assertions.assertTrue(actualBodyString.contains("\"gitCommitHash\":"), "Body should contain gitCommitHash element");
+    Assertions.assertTrue(actualBodyString.contains("\"gitCommitDatetime\":"), "Body should contain gitCommitDatetime element");
+    Assertions.assertTrue(actualBodyString.contains("\"gitCommitTags\":"), "Body should contain gitCommitTags element");
+    Assertions.assertTrue(actualBodyString.contains("\"gitCommitBranch\":"), "Body should contain gitCommitBranch element");
+    Assertions.assertTrue(actualBodyString.contains("\"mvnBuildDatetime\":"), "Body should contain mvnBuildDatetime element");
   }
 
   private MockEndpoint getMockEndpoint(String uri) {
